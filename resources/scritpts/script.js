8f4914109acbe1c342030aba6b1b9b44b03c5d9f
Kovrizhkin-ya <kovrizhkinaleksandr95@gmail.com>
2023-06-14 14:04:57 +0300

    Add files via upload

--- resources/scritpts/script.js
+++ resources/scritpts/script.js
@@ -1,11 +1,11 @@
-// Get the form element
+
 var form = document.querySelector('form');
 
-// Add event listener for form submission
-form.addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent form submission
 
-  // Get the input field values
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); 
+
+
   var nameInput = document.getElementById('name');
   var emailInput = document.getElementById('email');
   var messageInput = document.getElementById('message');
@@ -17,36 +17,31 @@ form.addEventListener('submit', function(event) {
     return;
   }
 
-  // Process the form data
-  // You can perform any necessary operations with the form data here
-  // For example, sending the data to a server using AJAX or performing client-side validation
 
-  // Clear the form inputs
   nameInput.value = '';
   emailInput.value = '';
   messageInput.value = '';
   countryInput.value = '';
 
-  // Display success message
   displaySuccessMessage('Form submitted successfully.');
 });
 
-// Function to display an error message
+
 function displayErrorMessage(message) {
   var errorDiv = document.createElement('div');
   errorDiv.classList.add('error-message');
   errorDiv.textContent = message;
 
-  // Append the error message to the form
+
   form.appendChild(errorDiv);
 }
 
-// Function to display a success message
+
 function displaySuccessMessage(message) {
   var successDiv = document.createElement('div');
   successDiv.classList.add('success-message');
   successDiv.textContent = message;
 
-  // Append the success message to the form
+
   form.appendChild(successDiv);
 }
